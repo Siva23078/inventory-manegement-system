bb7a428faafa01529d475b7f7affaa4ab6d82aeb
Siva23078 <siva.k23078@gmail.com>
2025-03-04 22:44:20 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,24 @@ dotenv.config();
 
 
 const app = express();
-app.get("/Inventorymanagementsystem" ,(req, res)=>{});
+app.post("/api/products", async (req, res)=>{
+
+  const product = req.body; // user body will send this data
+  
+  if(!product.name || !product.price){
+    return res.status(400).json({success:false,messasge:"please provid all fields  "});
+   }
+        const newProduct = new Product(product);
+
+        try{
+          await newProduct.save();
+        res.status(201).json({success:true , data:newProduct});
+        } catch (error){
+        console.error("Error in  Create Product:", error.messasge);
+          res.status(500).json({success:false, messasge:"Server Error"});
+        };
+       });
+        
 console.log(process.env.MONGO_URI);
 
 app.listen(5000,()=>{
